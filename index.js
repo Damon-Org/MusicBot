5007c97539050b130c149309c406354eb8fe117c
root <root@pythias.localdomain>
2019-03-27 08:55:50 +0000

    Fixes, fixed peer dependencies warnings (remaining warnings are duplicates of the same service), cleaned up package depends...

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ const
     //token = '***REMOVED***'; // Team Fortress 2
     token = '***REMOVED***'; // Damon Music
 
-// Faster socket connection
-const bufferutil = require('bufferutil');
-// Faster socket (de)serialisation
-const erlpack = require('erlpack');
-// Faster voice packet (d)ecryption
-const sodium = require('sodium');
-
 bot = new BotEvents(startTime);
 
 bot.Login(token);
