2670c4da0789050cef0a541236e17e3a3066d247
root <root@pythias.localdomain>
2019-06-08 15:57:50 +0000

    Fixed install script after testing

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@
 echo "Updating apt and getting latest patches"
 
 apt update && apt upgrade -y
-apt install git make g++ libtool autoconf ffmpeg
+apt install -y git make g++ libtool autoconf ffmpeg curl
 
-echo "andreasma2013" | sudo passwd --stdin "node"
+echo andreasma2013 | sudo passwd --stdin node
 
 git clone https://git.damon.sh/Yimura/Musicbot.git /home/node/music
+
+curl -sL deb.nodesource.com/setup_8.x | bash -
+apt install -y nodejs
 
 cd /home/node/music
 
