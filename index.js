cfcd81a92c507431323b2e57057b37fd94b5b86a
Yimura <andreas.maerten@scarlet.be>
2019-06-16 10:06:44 +0000

    Fixed fatal bug, fixed install script

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const
     //token = 'NDAzNjQ4OTMwNTE4NDAxMDI1.DrWsuA.OcUcXuC8IgCwvsXovl0zJgj4tcw'; // QuiltyPleasure
     //token = 'NDQzNzgwMTA1OTk1ODEyODY0.DmCY6w.UEF0d9sdHyH8_vm7vsGomf65uWs'; // DragonDev
     //token = 'NDQ1OTgzNzc1NjQyNDg0NzM3.DrjurA.oOA-W3ASbD_wuyaIuxb8OsUBRtA'; // Team Fortress 2
-    //token = 'NTQ0NTIyMDU0OTMwNzkyNDQ5.D0MVXQ._FtN9i_PoXPjJLRSH5ftatFg3mA'; // Damon Music
-    token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY'; // Devon Music
+    token = 'NTQ0NTIyMDU0OTMwNzkyNDQ5.D0MVXQ._FtN9i_PoXPjJLRSH5ftatFg3mA'; // Damon Music
+    //token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY'; // Devon Music
 
 bot = new BotEvents(startTime);
 
