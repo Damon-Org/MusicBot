3b72d8185d9bc3e9a192eae234f1a15c739913d6
Yimura <andreas.maerten@scarlet.be>
2019-08-09 12:46:50 +0200

    Initial commit for v3

--- index.js
+++ index.js
@@ -1,49 +1,13 @@
-'use strict';
+const ShardManager = require('discord.js').ShardingManager;
+const token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY';
 
-global.PATH = '/home/node/music';
-const
-    startTime = new Date(),
-    { HandleException } = require('./lib/functions.js'),
-    { BotEvents } = require('./lib/botevents.js'),
-    //token = 'NDAyMjQ2NTA5MzEzMzkyNjQw.DT2PIA.W9WqU027YvKV3kLLQbjRaiUdid8'; // Dragon
-    //token = 'NDAzNjQ4OTMwNTE4NDAxMDI1.DrWsuA.OcUcXuC8IgCwvsXovl0zJgj4tcw'; // QuiltyPleasure
-    //token = 'NDQzNzgwMTA1OTk1ODEyODY0.DmCY6w.UEF0d9sdHyH8_vm7vsGomf65uWs'; // DragonDev
-    //token = 'NDQ1OTgzNzc1NjQyNDg0NzM3.DrjurA.oOA-W3ASbD_wuyaIuxb8OsUBRtA'; // Team Fortress 2
-    token = 'NTQ0NTIyMDU0OTMwNzkyNDQ5.D0MVXQ._FtN9i_PoXPjJLRSH5ftatFg3mA'; // Damon Music
-    //token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY'; // Devon Music
+const Manager = new ShardManager('main.js', {
+    token: token,
+    respawn: false
+});
 
-const bot = new BotEvents(startTime);
+Manager.spawn();
 
-bot.Login(token);
-
-process
-    .on('SIGINT', () => {
-        console.log('\nBot shutdown requested logging out...');
-
-        setTimeout(function () {
-            bot.Logout();
-            console.log('Bot logout done!');
-            process.exit();
-        }, 500);
-    })
-
-    .on('SIGTERM', () => {
-        console.log('\nBot shutdown requested through SERVICE');
-
-        setTimeout(function () {
-            bot.Logout();
-            console.log('Bot logout done!');
-            process.exit();
-        }, 500);
-    })
-
-    .on('uncaughtException', err => {
-        HandleException(err);
-
-        setTimeout(function () {
-            console.log('Fatal error occured, logging bot out.');
-            bot.Logout();
-            console.log('Bot logout done!');
-            process.exit();
-        }, 500);
-  });
+Manager.on('launch', shard => {
+    console.log(`Shard ${shard.id + 1}/${Manager.totalShards} has started.`);
+});
