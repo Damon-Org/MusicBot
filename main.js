3a658b1c4874d2add46686b65aadd69b61d100a2
Yimura <andreas.maerten@scarlet.be>
2019-09-02 22:45:26 +0200

    Queue working partly, Music::Play command almost done

--- main.js
+++ main.js
@@ -3,3 +3,35 @@ const
     token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY';
 
 const instance = new MusicBot(token);
+
+process
+    .on('SIGINT', () => {
+        console.log('\nBot shutdown requested logging out...');
+
+        setTimeout(function () {
+            instance.logout();
+            console.log('Bot logout done!');
+            process.exit();
+        }, 500);
+    })
+
+    .on('SIGTERM', () => {
+        console.log('\nBot shutdown requested through SERVICE');
+
+        setTimeout(function () {
+            instance.logout();
+            console.log('Bot logout done!');
+            process.exit();
+        }, 500);
+    })
+
+    .on('uncaughtException', err => {
+        HandleException(err);
+
+        setTimeout(function () {
+            console.log('Fatal error occured, logging bot out.');
+            instance.logout();
+            console.log('Bot logout done!');
+            process.exit();
+        }, 500);
+  });
