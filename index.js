7f242421864088adc749d18b128a3643712afb1d
Yimura <andreas.maerten@scarlet.be>
2019-10-18 15:20:02 +0200

    Pushing README

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const ShardManager = require('discord.js').ShardingManager;
-const token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY';
+'use strict';
+
+const
+    token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY',
+    ShardManager = require('discord.js').ShardingManager,
+    ExternalCommunication = require('./lib/external/communication.js');
+
+const communication = new ExternalCommunication();
 
 const Manager = new ShardManager('main.js', {
     token: token,
