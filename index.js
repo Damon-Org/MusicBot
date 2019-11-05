c113d351de91bdc1bed2afad9634481bb62bf9ab
Yimura <andreas.maerten@scarlet.be>
2019-11-05 01:24:16 +0100

    Seperated the socket server from the bots and improved connection handling

--- index.js
+++ index.js
@@ -2,21 +2,15 @@
 
 const
     token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY',
-    ShardManager = require('discord.js').ShardingManager,
-    ExternalCommunication = require('./lib/external/communication.js');
+    ShardManager = require('discord.js').ShardingManager;
 
-const externalCommunication = new ExternalCommunication();
+const Manager = new ShardManager('main.js', {
+    token: token,
+    respawn: false
+});
 
-// Wait for socket server to be ready
-externalCommunication.socketServer.on('bind', () => {
-    const Manager = new ShardManager('main.js', {
-        token: token,
-        respawn: false
-    });
+Manager.spawn();
 
-    Manager.spawn();
-
-    Manager.on('launch', shard => {
-        console.log(`Shard ${shard.id + 1}/${Manager.totalShards} has started.`);
-    });
+Manager.on('launch', shard => {
+    console.log(`Shard ${shard.id + 1}/${Manager.totalShards} has started.`);
 });
