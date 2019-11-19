91163cd343aa1d7b9931f8116d94efc3fe987fcf
Yimura <andreas.maerten@scarlet.be>
2019-11-19 16:57:18 +0100

    Added resetprefix command

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const
-    token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY',
+    //token = 'NTQ0NTIyMDU0OTMwNzkyNDQ5.XdExdA.-j0hmybYVhANIqhK6GPIY0BVXn4', // Damon Music
+    token = 'NTY3NzM2ODE5MzA3NzczOTYy.XQP_2w.nIbgKE5z8Lul5fEDXM3_askeMJY', // Devon Music
     ShardManager = require('discord.js').ShardingManager;
 
 const Manager = new ShardManager('main.js', {
