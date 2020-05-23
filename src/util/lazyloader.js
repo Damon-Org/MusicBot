1f6278122c8133d6359d714b6c2d348fb7489244
Yimura <andreas.maerten@scarlet.be>
2020-05-23 18:11:54 +0200

    LazyLoader: Dynamic guild settings have been added

--- src/util/lazyloader.js
+++ src/util/lazyloader.js
@@ -1,3 +1,5 @@
+const guildSettings = require('../../data/guild_settings.json');
+
 class LazyLoader {
     /**
      * @param {DamonBase} db
@@ -7,23 +9,55 @@ class LazyLoader {
 
         this.ready = false;
 
-        this.prefixes = new Map();
+        this.settings = new Map();
 
         this.start();
     }
 
-    async loadPrefixes() {
-        const [rows, fields] = await this.db.db.query('SELECT core_guilds.serverId, core_settings.value FROM core_settings INNER JOIN core_guilds ON core_guilds.guild_id=core_settings.guild_id WHERE core_settings.option_id = 2');
+    getOption(option) {
+        if (isNaN(option)) {
+            throw new Error('Get option expects a numeral instead of a string.');
+        }
 
-        rows.forEach((row) => {
-            this.prefixes.set(row.serverId, row.value);
-        });
+        return this.db.db.query('SELECT core_guilds.serverId, core_settings.value FROM core_settings INNER JOIN core_guilds ON core_guilds.guild_id=core_settings.guild_id WHERE core_settings.option_id = ?', option);
+    }
+
+    get(guild_id, option_name) {
+        const settings = this.settings.get(guild_id);
+        if (!settings) return undefined;
+        return settings[option_name];
+    }
+
+    async loadSettings(guildSettings) {
+        for (let setting of guildSettings) {
+            const [rows, fields] = await this.getOption(setting.id);
+
+            for (let row of rows) {
+                this.set(row.serverId, setting.name, row.value);
+            }
+        }
+    }
+
+    set(guild_id, option, value) {
+        if (this.settings.has(guild_id)) {
+            const settings = this.settings.get(guild_id);
+            settings[option] = value;
+
+            this.settings.set(guild_id, settings);
+
+            return;
+        }
+
+        const settings = {};
+        settings[option] = value;
+
+        this.settings.set(guild_id, settings);
     }
 
     async start() {
         this.db.log('LAZY', 'INFO', 'LazyLoader has started.');
 
-        await this.loadPrefixes();
+        await this.loadSettings(guildSettings);
 
         this.ready = true;
 
