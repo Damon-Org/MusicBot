08d023ed52d43b9ede285aec98272802ce4e0939
Yimura <andreas.maerten@scarlet.be>
2020-07-02 18:10:52 +0200

    EntryPoint: Added SIGTERM shutdown

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import { ShardingManager } from 'discord.js'
 import auth from './data/auth.js'
 import config from './data/config.js'
+import fs from 'fs'
 
 const shardManager = new ShardingManager(`${process.cwd()}/src/main.js`, {
     token: config.development ? auth.token.dev : auth.token.prod,
     respawn: !config.development,
     shardArgs: [
-        process.env.npm_package_version
+        JSON.parse(fs.readFileSync(`${process.cwd()}/package.json`)).version
     ]
 });
 
 shardManager.spawn();
 
 shardManager.on('shardCreate', shard => console.log(`[SHARD_MANAGER] Shard ${shard.id + 1}/${shardManager.totalShards} is starting...`));
+
+const shutdown = async () => {
+    await shardManager.killAll();
+
+    process.exit();
+};
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
