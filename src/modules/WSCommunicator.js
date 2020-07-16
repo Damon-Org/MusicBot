90037df9c41d6bf6b582046ea557dc24e911fb39
Yimura <andreas.maerten@scarlet.be>
2020-07-16 16:16:52 +0200

    WSCommunicator: Added sendEvent and sendReply methods

--- src/modules/WSCommunicator.js
+++ src/modules/WSCommunicator.js
@@ -1,31 +1,101 @@
 import EventModule from '../structures/modules/EventModule.js'
 import log from '../util/Log.js'
-
-import WSUtil from './ws/Util.js'
+import { OPCodes, TargetTypes } from '../util/Constants.js'
+import { v4 as uuidv4 } from 'uuid'
 import WSClient from './ws/Client.js'
 
 export default class WSCommunicator extends EventModule {
     /**
-     * @param {MainClient} mainClient
+     * @param {Main} main
      */
-    constructor(mainClient) {
-        super(mainClient);
+    constructor(main) {
+        super(main);
 
         this.register(WSCommunicator, {
             name: 'ws'
         });
-
-        this.util = new WSUtil(this);
     }
 
     /**
      * @returns {boolean}
      */
     get connected() {
-        return this.client.connected;
+        return this._client.connected;
     }
 
     setup() {
-        this.client = new WSClient(this);
+        this._client = new WSClient(this);
+
+        this._client.on('event', (...args) => this.emit('event', ...args));
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {string} targetType
+     * @param {string} [targetIdentifier=null]
+     * @param {JSON} [data=null]
+     * @param {boolean} [collectResponse=false]
+     * @param {number} [timeout=1e3]
+     * @returns {boolean|Promise<Array>} Returns a true/false or Promise with an array of responses if collectResponse is set to true
+     */
+    sendEvent(eventName, targetType, targetIdentifier = null, data = null, collectResponse = false, timeout = 1e3) {
+        if (!TargetTypes[targetType]) {
+            throw new Error('Unknown target type was given.');
+
+            return false;
+        }
+
+        if (TargetTypes['GLOBAL'] !== TargetTypes[targetType] && TargetTypes['REPLY'] !== TargetTypes[targetType] && !targetIdentifier) {
+            throw new Error('Only target type GLOBAL and REPLY can take no target identifier.');
+
+            return false;
+        }
+
+        const pl = {
+            op: OPCodes['EVENT'],
+            d: data,
+            e: eventName,
+            i: {
+                k: TargetTypes[targetType],
+                id: targetIdentifier
+            }
+        };
+
+        if (!collectResponse) return this._client.send(pl);
+
+        pl.u = uuidv4();
+        this._client.send(pl);
+
+        let response = [];
+        return new Promise((resolve, reject) => {
+            const eventListener = this._client[pl.u];
+
+            const _timeout = setTimeout(() => {
+                resolve(response);
+            }, timeout);
+
+            eventListener.on('response', (res) => {
+                response.push(res.d);
+            });
+
+            eventListener.on('close', () => {
+                resolve(response);
+
+                clearTimeout(_timeout);
+                eventListener.close();
+            });
+        });
+    }
+
+    /**
+     * @param {string} id
+     * @param {JSON} data
+     */
+    sendReply(id, data) {
+        return this._client.send({
+            u: id,
+            op: OPCodes['REPLY'],
+            d: data
+        });
     }
 }
