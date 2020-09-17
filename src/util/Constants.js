52e3590f144db3ee58b5ed843c4e41f5714b6a65
Yimura <andreas.maerten@scarlet.be>
2020-09-17 11:58:34 +0200

    Constants: Updated Disconnect and OP codes

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,6 +1,7 @@
 export const DisconnectCodes = {
     'MESSAGE_TOO_BIG': 1009,
     'SERVER_RESTART': 1012,
+    'TRY_AGAIN_LATER': 1013,
 
     'UNKNOWN_ERROR': 4000,
     'UNKNOWN_OP': 4001,
@@ -23,7 +24,9 @@ export const OPCodes = {
     'IDENTIFY': 2,
     'BONJOUR': 3,
     'COMMUNICATION_CLOSE': 4,
-    'REPLY': 5
+    'REPLY': 5,
+    'PING': 6,
+    'PONG': 7
 };
 
 export const TargetTypes = {
