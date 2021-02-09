7bd79b6ba1fb1d17edd73dbde73837ad9074c673
Yimura <andreas.maerten@scarlet.be>
2021-02-09 13:30:29 +0100

    feat(Main): Removed root_dir, use path.resolve

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,12 @@
 import Main from './client/Main.js';
 import log from './util/Log.js'
 
-const instance = new Main(process.cwd(), process.env.DISCORD_TOKEN);
+const instance = new Main(process.env.DISCORD_TOKEN);
 instance.start();
 
 process.on('unhandledRejection', (err) => {
     if (!err.ignore) log.error('PROCESS', 'Error Occured:', err);
 });
 
-process.on('SIGINT', () => instance.shutdown());
-process.on('SIGTERM', () => instance.shutdown());
+process.on('SIGINT', () => instance.shutdown('SIGINT'));
+process.on('SIGTERM', () => instance.shutdown('SIGTERM'));
