812de742557e61b8ca65d52a13784c12ee8bf9a7
Yimura <andreas.maerten@scarlet.be>
2021-10-06 11:45:28 +0200

    feat(Modules): Added presence module

--- data/config.js
+++ data/config.js
@@ -5,8 +5,35 @@ export default {
     creator: "243072972326305798",
     permission_bit: 238348632,
 
+    allow_mention_prefix: true,
     default_prefix: {
         dev: 's!',
         prod: 'd!'
+    },
+
+    presence_settings: {
+        switch_interval: 25e3,
+        activities: [
+            {
+                type: "WATCHING",
+                name: "s!help"
+            },
+            {
+                type: "WATCHING",
+                name: "version ${version}"
+            },
+            {
+                type:  "WATCHING",
+                name: "us on spyral.gg"
+            },
+            {
+                type:  "PLAYING",
+                name: "with all the commands on commands.spyral.gg"
+            },
+            {
+                type:  "WATCHING",
+                name: "over ${serverCount} guilds"
+            }
+        ]
     }
 };
